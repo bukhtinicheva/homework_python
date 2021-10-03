f35a62ad1dfd4b62f0f766c8973b1efb8423b1f8
Kseniia Bukhtinicheva <89969036+bukhtinicheva@users.noreply.github.com>
2021-10-03 12:33:55 +0300

    Delete task10.09.py

--- task10.09.py
+++
@@ -1,6 +0,0 @@
-def show(func):
-    def new_func(*args, **kwargs):
-        print('Name of function - ', func.__name__)
-        print('args - ', args)
-        print('kwargs - ', kwargs)
-    return new_func
